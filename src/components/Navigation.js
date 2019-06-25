21f356a56c825378e8f3a46c6dbbb40693025341
Ethan Duffy <ethan.md@gmail.com>
2019-06-25 15:45:15 +0200

    move text

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -87,7 +87,7 @@ export const Navigation = () => (
   <NavigationWrapper>
     <Logo><a href="/">
       ETHAN <span css="letter-spacing: 1px;">DUFFY</span></a>	
-<a href="/de/"><img src={flags} ALIGN="right" style="margin: 10px 5px 15px 20px;"></img></a>
+<a href="/de/"><img src={flags} style="margin: 10px 5px 15px 20px;" ALIGN="right"></img></a>
 </Logo>
     <NavigationLinks>
       <MainLink to="/factual">factual</MainLink>
