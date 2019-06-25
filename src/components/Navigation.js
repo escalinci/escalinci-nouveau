d2f6bef93b0ce85438262a4cea02e79a03b0741e
Ethan Duffy <ethan.md@gmail.com>
2019-06-25 14:23:44 +0200

    link change

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -85,10 +85,10 @@ const Contact = styled.div`
 
 export const Navigation = () => (
   <NavigationWrapper>
-    <Logo to ="/">
-      ETHAN <span css="letter-spacing: 1px;">DUFFY</span>	</Logo>
+    <Logo><a href=="/">
+      ETHAN <span css="letter-spacing: 1px;">DUFFY</span></a>	
 <a href="/de/"><img src={flags} ALIGN="right"></img></a>
-	  <p></p>
+</Logo>
     <NavigationLinks>
       <MainLink to="/factual">factual</MainLink>
       <MainLink to="/scripted">scripted</MainLink>
