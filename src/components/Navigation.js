226bc7c1621a6ae797c6390d955f1dac51f84e9d
Ethan Duffy <ethan.md@gmail.com>
2019-06-25 16:08:59 +0200

    Doth link now worketh

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -97,7 +97,7 @@ export const Navigation = () => (
     <Logo to="/">
       ETHAN <span css="letter-spacing: 1px;">DUFFY</span>	
 </Logo>
-		<flagwrapper to="/de/"><img src={flags} ALIGN="right"></img></flagwrapper>
+		<flagwrapper><a href="/de/"><img src={flags} ALIGN="right"></img></flagwrapper></a>
     <NavigationLinks>
       <MainLink to="/factual">factual</MainLink>
       <MainLink to="/scripted">scripted</MainLink>
