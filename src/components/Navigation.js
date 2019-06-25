95adbee57c440273a731f2ad1c59353ccd9dd631
Ethan Duffy <ethan.md@gmail.com>
2019-06-25 14:49:34 +0200

    Is this acceptable

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -85,7 +85,7 @@ const Contact = styled.div`
 
 export const Navigation = () => (
   <NavigationWrapper>
-    <Logo font-style: thin;><a href="/">
+    <Logo style="font-style: thin;"><a href="/">
       ETHAN <span css="letter-spacing: 1px;">DUFFY</span></a>	
 <a href="/de/"><img src={flags} ALIGN="right"></img></a>
 </Logo>
